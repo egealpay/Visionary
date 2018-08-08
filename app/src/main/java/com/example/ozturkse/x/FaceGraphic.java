9c6687a0e6ef34a3934a96a508bfdc1125e1eb52
Selim Öztürk <selim.ozturk@commencis.com>
2018-08-08 16:12:33 +0300

    Add happy/sad
    Add architecture UMLs

--- app/src/main/java/com/example/ozturkse/x/FaceGraphic.java
+++ app/src/main/java/com/example/ozturkse/x/FaceGraphic.java
@@ -26,85 +26,102 @@ import com.google.firebase.ml.vision.face.FirebaseVisionFace;
  * graphic overlay view.
  */
 public class FaceGraphic extends GraphicOverlay.Graphic {
-  private static final float ID_TEXT_SIZE = 64.0f;
-  private static final float ID_Y_OFFSET = 50.0f;
-  private static final float ID_X_OFFSET = -50.0f;
-  private static final float BOX_STROKE_WIDTH = 5.0f;
+    private static final float ID_TEXT_SIZE = 64.0f;
+    private static final float ID_Y_OFFSET = 50.0f;
+    private static final float ID_X_OFFSET = 50.0f;
+    private static final float BOX_STROKE_WIDTH = 5.0f;
 
-  private static final int[] COLOR_CHOICES = {
-    Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA, Color.RED, Color.WHITE, Color.YELLOW
-  };
-  private static int currentColorIndex = 0;
+    private static final int[] COLOR_CHOICES = {
+            Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA, Color.RED, Color.WHITE, Color.YELLOW
+    };
+    private static int currentColorIndex = 0;
 
-  private int facing;
+    private int facing;
 
-  private final Paint facePositionPaint;
-  private final Paint idPaint;
-  private final Paint boxPaint;
+    private final Paint facePositionPaint;
+    private final Paint idPaint;
+    private final Paint boxPaint;
 
-  private volatile FirebaseVisionFace firebaseVisionFace;
+    private volatile FirebaseVisionFace firebaseVisionFace;
 
-  public FaceGraphic(GraphicOverlay overlay) {
-    super(overlay);
+    public FaceGraphic(GraphicOverlay overlay) {
+        super(overlay);
 
-    currentColorIndex = (currentColorIndex + 1) % COLOR_CHOICES.length;
-    final int selectedColor = COLOR_CHOICES[currentColorIndex];
+        currentColorIndex = (currentColorIndex + 1) % COLOR_CHOICES.length;
+        final int selectedColor = COLOR_CHOICES[currentColorIndex];
 
-    facePositionPaint = new Paint();
-    facePositionPaint.setColor(selectedColor);
+        facePositionPaint = new Paint();
+        facePositionPaint.setColor(selectedColor);
 
-    idPaint = new Paint();
-    idPaint.setColor(selectedColor);
-    idPaint.setTextSize(ID_TEXT_SIZE);
+        idPaint = new Paint();
+        idPaint.setColor(selectedColor);
+        idPaint.setTextSize(ID_TEXT_SIZE);
 
-    boxPaint = new Paint();
-    boxPaint.setColor(selectedColor);
-    boxPaint.setStyle(Paint.Style.STROKE);
-    boxPaint.setStrokeWidth(BOX_STROKE_WIDTH);
-  }
-
-  /**
-   * Updates the face instance from the detection of the most recent frame. Invalidates the relevant
-   * portions of the overlay to trigger a redraw.
-   */
-  public void updateFace(FirebaseVisionFace face, int facing) {
-    firebaseVisionFace = face;
-    this.facing = facing;
-    postInvalidate();
-  }
-
-  /** Draws the face annotations for position on the supplied canvas. */
-  @Override
-  public void draw(Canvas canvas) {
-    FirebaseVisionFace face = firebaseVisionFace;
-    if (face == null) {
-      return;
+        boxPaint = new Paint();
+        boxPaint.setColor(selectedColor);
+        boxPaint.setStyle(Paint.Style.STROKE);
+        boxPaint.setStrokeWidth(BOX_STROKE_WIDTH);
     }
 
-    MainActivity.Companion.recognizeFace();
+    /**
+     * Updates the face instance from the detection of the most recent frame. Invalidates the relevant
+     * portions of the overlay to trigger a redraw.
+     */
+    public void updateFace(FirebaseVisionFace face, int facing) {
+        firebaseVisionFace = face;
+        this.facing = facing;
+        postInvalidate();
+    }
 
-    float x = translateX(face.getBoundingBox().centerX());
-    float y = translateY(face.getBoundingBox().centerY());
-    canvas.drawText(
-        "happiness: " + String.format("%.2f", face.getSmilingProbability()),
-        x + ID_X_OFFSET * 7,
-        y - ID_Y_OFFSET * 7 ,
-        idPaint);
-    canvas.drawText(
-            MainActivity.Companion.getResponseName(),
-            x - ID_X_OFFSET * 6,
-            y - ID_Y_OFFSET * 6,
-            idPaint
-    );
+    /**
+     * Draws the face annotations for position on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        FirebaseVisionFace face = firebaseVisionFace;
+        if (face == null) {
+            return;
+        }
 
-    // Draws a bounding box around the face.
-    float xOffset = scaleX(face.getBoundingBox().width() / 2.0f);
-    float yOffset = scaleY(face.getBoundingBox().height() / 2.0f);
-    float left = x - xOffset;
-    float top = y - yOffset;
-    float right = x + xOffset;
-    float bottom = y + yOffset;
-    canvas.drawRect(left, top, right, bottom, boxPaint);
-  }
+        MainActivity.Companion.recognizeFace();
+
+        float x = translateX(face.getBoundingBox().centerX());
+        float y = translateY(face.getBoundingBox().centerY());
+
+        canvas.drawText(
+                MainActivity.Companion.getResponseName(),
+                x,
+                y - ID_Y_OFFSET * 5,
+                idPaint
+        );
+
+        int smilingProbability = Math.round(face.getSmilingProbability() * 100);
+
+        if (smilingProbability < 0) {
+            canvas.drawText(
+                    "sad: " + smilingProbability * -1,
+                    x,
+                    y + ID_Y_OFFSET * 5,
+                    idPaint
+            );
+        } else {
+            canvas.drawText(
+                    "happy: " + smilingProbability,
+                    x,
+                    y + ID_Y_OFFSET * 5,
+                    idPaint
+            );
+        }
+
+
+        // Draws a bounding box around the face.
+        float xOffset = scaleX(face.getBoundingBox().width() / 2.0f);
+        float yOffset = scaleY(face.getBoundingBox().height() / 2.0f);
+        float left = x - xOffset;
+        float top = y - yOffset;
+        float right = x + xOffset;
+        float bottom = y + yOffset;
+        canvas.drawRect(left, top, right, bottom, boxPaint);
+    }
 
 }
