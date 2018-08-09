d0ee99c38ae364eed674ecf9103671cbf2cf2ac2
Selim Öztürk <selim.ozturk@commencis.com>
2018-08-09 15:11:20 +0300

    Check for last seen person first
    Simplify methods

--- backend/webapp/util.py
+++ backend/webapp/util.py
@@ -1,9 +1,8 @@
-import face_recognition
 import os
 import pickle
-from io import BytesIO
 
 model_path = "known_faces"
+
 
 def get_known_faces():
     known_faces = dict()
@@ -24,15 +23,3 @@ def save_dict(updated_known_faces):
             pickle.dump(updated_known_faces, f, protocol=pickle.HIGHEST_PROTOCOL)
     except IOError as e:
         print(e)
-
-
-def get_face_encoding(photo):
-    face_encoding = None
-    try:
-        image_bytes = photo.read()
-        stream = BytesIO(image_bytes)
-        photo_array = face_recognition.load_image_file(stream)
-        face_encoding = face_recognition.face_encodings(photo_array)[0]
-    except IndexError as e:  # couldn't locate a face
-        print(e)
-    return face_encoding
