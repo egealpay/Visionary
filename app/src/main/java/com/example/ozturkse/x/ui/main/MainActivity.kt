e2e08748d9157131e3091472db13aca0717826e0
Selim Öztürk <selim.ozturk@commencis.com>
2018-08-10 10:36:49 +0300

    Replace static strings with resources
    Add localization

--- app/src/main/java/com/example/ozturkse/x/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/ozturkse/x/ui/main/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), MainView {
 
         fun recognizeFace() {
             if (!Util.isInternetAvailable(appContext)) {
-                Toast.makeText(appContext, "No internet connection", Toast.LENGTH_SHORT).show()
+                Toast.makeText(appContext, R.string.no_connection, Toast.LENGTH_SHORT).show()
                 return
             }
 
@@ -185,7 +185,7 @@ class MainActivity : AppCompatActivity(), MainView {
                             val builder = AlertDialog.Builder(this@MainActivity)
                             builder.setTitle(":(")
                             builder.setMessage(
-                                    "In order to use this app, you should give permission to use camera, from Settings!"
+                                    R.string.give_camera_permission
                             )
 
                             val dialog: AlertDialog = builder.create()
